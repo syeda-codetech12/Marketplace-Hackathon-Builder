8e4abf0ba6334134ac7e7789f3a57ef7b8893498
Syeda Ayesha <sbs86.freelancer@gmail.com>
2025-02-10 19:40:39 +0500

    Changes uploaded

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -2,7 +2,7 @@ export default function NotFound() {
     return (
       <div className="flex flex-col items-center justify-center h-screen">
         <h1 className="text-4xl font-bold">404 - Page Not Found</h1>
-        <p className="text-lg mt-2">Oops! The page you're looking for doesn't exist.</p>
+        <p className="text-lg mt-2">Oops! The page you&apos;re looking for doesn&apos;t exist.</p>
       </div>
     );
   }
